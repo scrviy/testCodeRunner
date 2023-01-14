0ac885f4441da8194bbe9d0457716b7309f3d0ac
scrviy <91909966+scrviy@users.noreply.github.com>
2023-01-13 22:56:58 -0300

    teste

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TestCodeRunner
+{
+    public class TestCodeRunner
+    {
+        public static void Main(string[] args)
+        {
+            Console.Clear();
+            string? nome, sobrenome;
+            Console.Write("Insira seu nome: ");
+            nome = Console.ReadLine();
+            Console.Write("Insira seu sobrenome: ");
+            sobrenome = Console.ReadLine();
+            Console.WriteLine($"Seu nome completo é: {nome} {sobrenome}");
+        }
+    }
+}
